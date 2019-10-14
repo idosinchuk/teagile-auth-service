e9936394509a8451cfec9c390bf028f4cdab6878
idosinchuk <idosinchuk@ITEM-S84804.emea.msad.sopra>
2019-10-14 13:48:50 +0200

    Adding logs

--- teagile-auth-service/src/main/java/com/idosinchuk/teagile/auth/service/AuthApplication.java
+++ teagile-auth-service/src/main/java/com/idosinchuk/teagile/auth/service/AuthApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @EnableEurekaClient
 @SpringBootApplication
 public class AuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
+        log.info("-- AuthApplication started --");
 	}
 
 }
